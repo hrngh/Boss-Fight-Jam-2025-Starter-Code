f6824475696100d29c5cfb01f95fc86bdf3221b8
hrngh <josjamperc@gmail.com>
2025-07-17 17:48:04 +0300

    enemy health maybe

--- Assets/Starter Files/Scripts/EnemyHealth.cs
+++ Assets/Starter Files/Scripts/EnemyHealth.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
+public class EnemyHealth : MonoBehaviour
 {
 
     [Header("Health Modifications")]
@@ -14,23 +14,19 @@ public class PlayerHealth : MonoBehaviour
     public GameObject flashPrefab;
 
     // other vars
-    private Player playerScript;
-    private float health;
+    [HideInInspector] public float health;
     private float immunityTimer;
     [HideInInspector] public bool dead;
 
     void Start()
     {
-        // grab player script
-        TryGetComponent<Player>(out playerScript);
-        if (!playerScript) Debug.Log("Check that all PlayerHealths' gameobjects have an associated player");
-
+        // put any code here that you need (such as UI stuff)
     }
 
     void Update()
     {
         // update immunity frames
-        if(immunityTimer >= 0) immunityTimer -= Time.deltaTime;
+        if (immunityTimer >= 0) immunityTimer -= Time.deltaTime;
         health = maxHealth;
     }
 
@@ -39,16 +35,17 @@ public class PlayerHealth : MonoBehaviour
         // get the attack's script and ignore non-attacks
         Attack attackScript;
         collision.TryGetComponent<Attack>(out attackScript);
-        if (!attackScript || attackScript.friendly || playerScript.isInvulnerable()) return;
+        if (!attackScript || !attackScript.friendly) return;
 
         // try damage/immunity
-        if(immunityTimer <= 0)
+        if (immunityTimer <= 0)
         {
             health -= attackScript.damage;
             if (health < 0)
             {
                 die();
-            } else
+            }
+            else
             {
                 doSpriteFlash();
             }
@@ -75,10 +72,7 @@ public class PlayerHealth : MonoBehaviour
         // clamp health and let player know to die
         health = 0;
         dead = true;
-        playerScript.canInput = false;
-        // play death anim if enabled
-        if (playerScript.enableDieAnim) playerScript.anim.Play("die");
 
-        // you probably want to put more code here
+        // you probably want to put more code here (or somewhere else)
     }
 }
